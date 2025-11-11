9b15055b9bd655413846a905295f8d64bb433374
Sairaj21611 <sairajdeshmukh21611@gmail.com>
2025-11-11 16:55:36 +0530

    added files

--- LinkedList/src/com/linked/list/DoublyLinkedList.java
+++ LinkedList/src/com/linked/list/DoublyLinkedList.java
@@ -1,5 +1,143 @@
 package com.linked.list;
 
-public class DoublyLinkedList {
-
+public class DoublyLinkedList 
+{
+	Node head;
+	
+	class Node
+	{
+		int data;
+		Node prev;
+		Node next;
+		
+		public Node(int data)
+		{
+			this.data = data;
+			this.prev = null;
+			this.next = null;
+		}
+	}
+	
+	public DoublyLinkedList()
+	{
+		this.head = null;
+	}
+	
+	
+	public void addNode(int data)
+	{
+		Node newNode = new Node(data);
+		
+		if(head == null)
+		{
+			head = newNode ;
+		}
+		else
+		{
+			Node temp = head;
+			
+			while(temp.next != null)
+			{
+				temp = temp.next;
+			}
+			
+			temp.next = newNode;
+			newNode.prev = temp;
+		}
+	}
+	
+	
+	public void addByPos(int pos, int data)
+	{
+		Node newNode = new Node(data);
+		
+		if(pos == 1)
+		{
+			 newNode.next = head;
+			 
+			 if(head != null)
+			 {
+				 head.prev = newNode;
+			 }
+			 
+			 head = newNode;
+		}
+		
+		else
+		{
+			Node temp = head;
+			
+			for(int i=0; temp != null && i<pos-2; i++)
+			{
+				temp = temp.next;
+			}
+			
+			if(temp != null)
+			{
+				newNode.next = temp.next;
+				newNode.prev = temp;
+				temp.next = newNode;
+				
+				if(newNode.next != null)
+				{
+					newNode.next.prev = newNode;
+				}
+			}
+			else
+			{
+				System.out.println("Position beyond the length");
+			}
+			
+		}
+	}
+	
+	public void addAfterValue(int value, int data)
+	{
+		Node newNode = new Node(data);
+		
+		Node temp = head;
+		
+		while(temp != null && temp.data != value)
+		{
+			temp = temp.next;
+		}
+		
+		if(temp != null)
+		{
+			newNode.next = temp.next;
+			newNode.prev = temp;
+			temp.next = newNode;
+			
+			if(newNode.next != null)
+			{
+				newNode.next.prev = newNode;
+			}
+		}
+		else 
+		{
+			System.out.println("number Not found");
+		}
+		
+		
+	}
+	
+	public void display()
+	{
+		Node temp = head;
+		
+		while(temp != null)
+		{
+			System.out.print(temp.data+"--->");
+			temp = temp.next;
+		}
+		System.out.println();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 }
--- LinkedList/src/com/list/test/TestDoublyLinkedList.java
+++ LinkedList/src/com/list/test/TestDoublyLinkedList.java
@@ -1,10 +1,23 @@
 package com.list.test;
 
+import com.linked.list.DoublyLinkedList;
+
 public class TestDoublyLinkedList {
 
 	public static void main(String[] args) 
 	{
+		DoublyLinkedList list = new DoublyLinkedList();
 		
+		list.addNode(10);
+		
+		list.addByPos(2, 20);
+		list.addByPos(3, 30);
+		list.addByPos(4, 40);
+		list.addByPos(5, 50);
+		
+		list.addAfterValue(40, 45);
+//		
+		list.display();
 
 	}
 
